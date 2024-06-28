fc7155af7e17f69a6d4b3e5aabd67481976ddc97
= <=>
2024-06-28 17:43:15 +0530

    Code Fixed and Added New Function to Code by Indra

--- Final/finalcode.c
+++ Final/finalcode.c
@@ -18,18 +18,17 @@ int main()
     int choice;
     char input[10];
 
-        printf("\nLibrary Management System\n");
-        printf("1. Add Book\n");
-        printf("2. Display Available Books\n");
-        printf("3. Borrow Book\n");
-        printf("4. Return Book\n");
-        printf("5. Search Books by Title\n");
-        printf("6. Search Books by Author\n");
-        printf("0. Exit\n");
+    printf("\nLibrary Management System\n");
+    printf("1. Add Book\n");
+    printf("2. Display Available Books\n");
+    printf("3. Borrow Book\n");
+    printf("4. Return Book\n");
+    printf("5. Search Books by Title\n");
+    printf("6. Search Books by Author\n");
+    printf("0. Exit\n");
     do
     {
         printf("\nEnter your choice: ");
-
         scanf("%s", input);
         choice = -1;
 
@@ -76,11 +75,26 @@ int main()
     return 0;
 }
 
+int isOnlyDigits(char digit[])
+{
+    int i = 0;
+    while (digit[i] != '\0')
+    {
+        if (digit[i] < '0' || digit[i] > '9')
+        {
+            return 0;
+        }
+        i++;
+    }
+    return 1;
+}
+
 void addBook()
 {
     char tempTitle[100];
     char tempAuthor[100];
     int titleIsNumber, authorIsNumber;
+    int i;
 
     if (bookCount < 5)
     {
@@ -90,13 +104,15 @@ void addBook()
             printf("Enter book title: ");
             scanf(" %[^\n]%*c", tempTitle);
 
-            for (int i = 0; tempTitle[i] != '\0'; i++)
+            i = 0;
+            while (tempTitle[i] != '\0')
             {
                 if ((tempTitle[i] < '0' || tempTitle[i] > '9') && tempTitle[i] != ' ')
                 {
                     titleIsNumber = 0;
                     break;
                 }
+                i++;
             }
 
             if (titleIsNumber)
@@ -114,13 +130,15 @@ void addBook()
             printf("Enter author name: ");
             scanf(" %[^\n]%*c", tempAuthor);
 
-            for (int i = 0; tempAuthor[i] != '\0'; i++)
+            i = 0;
+            while (tempAuthor[i] != '\0')
             {
                 if (tempAuthor[i] >= '0' && tempAuthor[i] <= '9')
                 {
                     authorIsNumber = 1;
                     break;
                 }
+                i++;
             }
 
             if (authorIsNumber)
@@ -146,10 +164,11 @@ void addBook()
 void DisplayAvailableBooks()
 {
     int found = 0;
+    int i;
     printf("\n============================================");
     printf("\nTitle  \t \t            Author");
     printf("\n=============================================\n");
-    for (int i = 0; i < bookCount; i++)
+    for (i = 0; i < bookCount; i++)
     {
         if (availability[i])
         {
@@ -167,10 +186,21 @@ void DisplayAvailableBooks()
 void borrowBook()
 {
     char title[100];
-    printf("Please enter the title of the book you want to borrow: ");
-    scanf(" %[^\n]%*c", title);
+    int isDigits;
+    int i;
 
-    int i = 0;
+    do
+    {
+        printf("Please enter the title of the book you want to borrow: ");
+        scanf(" %[^\n]%*c", title);
+        isDigits = isOnlyDigits(title);
+        if (isDigits)
+        {
+            printf("Invalid input. Title cannot be only digits.\n");
+        }
+    } while (isDigits);
+
+    i = 0;
     while (i < bookCount)
     {
         if (strcmp(titles[i], title) == 0)
@@ -179,7 +209,10 @@ void borrowBook()
             {
                 availability[i] = 0;
                 printf("You have successfully borrowed:\n");
-                printf("Title: %s\nAuthor: %s\n", titles[i], authors[i]);
+                printf("\n============================================");
+                printf("\nTitle  \t \t            Author");
+                printf("\n=============================================\n");
+                printf("%s    \t \t           %s\n", titles[i], authors[i]);
                 return;
             }
             else
@@ -196,10 +229,21 @@ void borrowBook()
 void returnBook()
 {
     char title[100];
-    printf("Enter the title of the book to return: ");
-    scanf(" %[^\n]%*c", title);
+    int isDigits;
+    int i;
 
-    int i = 0;
+    do
+    {
+        printf("Enter the title of the book to return: ");
+        scanf(" %[^\n]%*c", title);
+        isDigits = isOnlyDigits(title);
+        if (isDigits)
+        {
+            printf("Invalid input. Title cannot be only digits.\n");
+        }
+    } while (isDigits);
+
+    i = 0;
     while (i < bookCount)
     {
         if (strcmp(titles[i], title) == 0)
@@ -208,7 +252,10 @@ void returnBook()
             {
                 availability[i] = 1;
                 printf("You returned the book:\n");
-                printf("Title: %s\nAuthor: %s\n", titles[i], authors[i]);
+                printf("\n============================================");
+                printf("\nTitle  \t \t            Author");
+                printf("\n=============================================\n");
+                printf("%s    \t \t           %s\n", titles[i], authors[i]);
                 return;
             }
             else
@@ -225,15 +272,30 @@ void returnBook()
 void searchBooksByTitle()
 {
     char title[100];
-    printf("Enter the title to search for: ");
-    scanf(" %[^\n]%*c", title);
-
+    int isDigits;
     int found = 0;
-    for (int i = 0; i < bookCount; i++)
+    int i;
+
+    do
+    {
+        printf("Enter the title to search for: ");
+        scanf(" %[^\n]%*c", title);
+        isDigits = isOnlyDigits(title);
+        if (isDigits)
+        {
+            printf("Invalid input. Title cannot be only digits.\n");
+        }
+    } while (isDigits);
+
+    for (i = 0; i < bookCount; i++)
     {
         if (strstr(titles[i], title) != NULL)
         {
-            printf("Title: %s\nAuthor: %s\nAvailable: %s\n", titles[i], authors[i], availability[i] ? "Yes" : "No");
+            printf("\n============================================");
+            printf("\nTitle  \t \t            Author");
+            printf("\n=============================================\n");
+            printf("%s    \t \t           %s\n", titles[i], authors[i]);
+            printf("Available: %s\n", availability[i] ? "Yes" : "No");
             found = 1;
         }
     }
@@ -246,15 +308,30 @@ void searchBooksByTitle()
 void searchBooksByAuthor()
 {
     char author[100];
-    printf("Enter the author to search for: ");
-    scanf(" %[^\n]%*c", author);
-
+    int isDigits;
     int found = 0;
-    for (int i = 0; i < bookCount; i++)
+    int i;
+
+    do
+    {
+        printf("Enter the author to search for: ");
+        scanf(" %[^\n]%*c", author);
+        isDigits = isOnlyDigits(author);
+        if (isDigits)
+        {
+            printf("Invalid input. Author name cannot be only digits.\n");
+        }
+    } while (isDigits);
+
+    for (i = 0; i < bookCount; i++)
     {
         if (strstr(authors[i], author) != NULL)
         {
-            printf("Title: %s\nAuthor: %s\nAvailable: %s\n", titles[i], authors[i], availability[i] ? "Yes" : "No");
+            printf("\n============================================");
+            printf("\nTitle  \t \t            Author");
+            printf("\n=============================================\n");
+            printf("%s    \t \t           %s\n", titles[i], authors[i]);
+            printf("Available: %s\n", availability[i] ? "Yes" : "No");
             found = 1;
         }
     }
