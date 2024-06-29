ca072dc6612c5ef20a8f949f367e7807cb71c30c
sfaiz801 <sfaiz777@rediffemail.com>
2024-06-29 16:21:16 +0530

    Faiz_updated Code this colour

--- Faiz_siddique/search_book_title_programing.c
+++ Faiz_siddique/search_book_title_programing.c
@@ -1,4 +1,3 @@
-#include <stdio.h>
 #include <string.h>
 
 char titles[5][100];
@@ -9,22 +8,35 @@ int bookCount = 0;
 void searchBooksByTitle()
 {
     char title[100];
-    printf("Enter the title to search for: ");
-    scanf(" %[^\n]%*c", title);
-
+    int isDigits;
     int found = 0;
-    int i = 0;
-    while (i < bookCount)
+    int i;
+
+    do
+    {
+        printf("Enter the title to search for: ");
+        scanf(" %[^\n]%*c", title);
+        isDigits = isOnlyDigits(title);
+        if (isDigits)
+        {
+            printf("\033[0;31mInvalid input. Title cannot be only digits.\033[0m\n");
+        }
+    } while (isDigits);
+
+    for (i = 0; i < bookCount; i++)
     {
         if (strstr(titles[i], title) != NULL)
         {
-            printf("Title: %s, Author: %s, Available: %s\n", titles[i], authors[i], availability[i] ? "Yes" : "No");
+            printf("\n============================================");
+            printf("\nTitle  \t \t            Author");
+            printf("\n=============================================\n");
+            printf("%s    \t \t           %s\n", titles[i], authors[i]);
+            printf("Available: %s\n", availability[i] ? "Yes" : "No");
             found = 1;
         }
-        i++;
     }
     if (!found)
     {
-        printf("No books found with the title containing '%s'.\n", title);
+        printf("\033[0;31mNo books found with the title containing '%s'.\033[0m\n", title);
     }
 }
