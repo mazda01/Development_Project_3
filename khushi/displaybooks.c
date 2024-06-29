de9a4a908e19f549be05dfabd625e74c6206c778
khus759 <khushikumarigupta@email.com>
2024-06-29 16:20:24 +0530

    modify with colour

--- khushi/displaybooks.c
+++ khushi/displaybooks.c
@@ -1,31 +1,21 @@
-void Displayavailablebooks();
-int main()
-{
-    DisplayAvailableBooks();
-    return 0;
-}
-
 void DisplayAvailableBooks()
 {
     int found = 0;
-    int i = 0;
-    while (i < bookCount)
+    int i;
+    printf("\n============================================");
+    printf("\nTitle  \t \t            Author");
+    printf("\n=============================================\n");
+    for (i = 0; i < bookCount; i++)
     {
         if (availability[i])
         {
-    
-            printf("Available Books: ");
-            printf("\n============================================");
-            printf("\nTitle  \t \t            Author");
-            printf("\n=============================================");
-            printf("\n%s    \t \t           %s\n", titles[i], authors[i]);
-            printf("===============================================");
+            printf("%s    \t \t           %s\n", titles[i], authors[i]);
             found = 1;
         }
-        i++;
     }
     if (!found)
     {
-        printf("No available books.\n");
+        printf("\033[0;31mNo available books.\033[0m\n");
     }
+    printf("===============================================\n");
 }
